15dde9cd175a71258d10010cabf7b934dc797bb1
Willy <Willytanuwijaya4@gmail.com>
2026-07-08 19:23:19 +0700

    Implement Joining Shared Zone

--- foodsteps/App/foodstepsApp.swift
+++ foodsteps/App/foodstepsApp.swift
@@ -1,14 +1,37 @@
 import CoreData
 import SwiftUI
+import CloudKit
 
 let dataController = DataController()
 
 @main
 struct foodstepsApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environment(\.managedObjectContext, dataController.container.viewContext)
         }
+    }
+}
+
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(
+        _: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions
+    )
+    -> UISceneConfiguration
+    {
+        let configuration = UISceneConfiguration(name: nil, sessionRole: connectingSceneSession.role)
+        configuration.delegateClass = SceneDelegate.self
+        return configuration
+    }
+}
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+
+    func windowScene(_ windowScene: UIWindowScene, userDidAcceptCloudKitShareWith shareMetadata: CKShare.Metadata) {
+        dataController.container.acceptShareInvitations(from: [shareMetadata], into: dataController.sharedPersistentStore)
     }
 }
