2b9e0c7c36d5ed9f2ef2714e96df4ebc8849f67e
HikarusV <nazwapradana.pc@gmail.com>
2026-07-09 00:22:21 +0700

    [feat]: extension Color class for support hex color

---
+++ foodsteps/Views/Components/ImageView/HexColor.swift
@@ -0,0 +1,46 @@
+//
+//  HexColor.swift
+//  strava-like
+//
+//  Created by Nazwa Sapta Pradana on 07/07/26.
+//
+
+import SwiftUI
+
+extension Color {
+    init(hex: String) {
+        let hex = hex.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
+
+        var int: UInt64 = 0
+        Scanner(string: hex).scanHexInt64(&int)
+
+        let r, g, b, a: UInt64
+
+        switch hex.count {
+        case 6: // RRGGBB
+            (r, g, b, a) = (
+                (int >> 16) & 0xFF,
+                (int >> 8) & 0xFF,
+                int & 0xFF,
+                255
+            )
+        case 8: // AARRGGBB
+            (a, r, g, b) = (
+                (int >> 24) & 0xFF,
+                (int >> 16) & 0xFF,
+                (int >> 8) & 0xFF,
+                int & 0xFF
+            )
+        default:
+            (r, g, b, a) = (0, 0, 0, 255)
+        }
+
+        self.init(
+            .sRGB,
+            red: Double(r) / 255,
+            green: Double(g) / 255,
+            blue: Double(b) / 255,
+            opacity: Double(a) / 255
+        )
+    }
+}
