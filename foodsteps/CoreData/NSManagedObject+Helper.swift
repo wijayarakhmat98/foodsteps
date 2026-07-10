aa6cf0debfbc0591792b445ed41fb4d69758a7cc
Wijaya Rakhmat <wijaya.rakhmat@hotmail.com>
2026-07-10 03:20:17 +0000

    Fix active participant list

--- foodsteps/CoreData/NSManagedObject+Helper.swift
+++ foodsteps/CoreData/NSManagedObject+Helper.swift
@@ -2,35 +2,43 @@ import CoreData
 import CloudKit
 
 private let erroneousUserIdentities = [CKUserIdentity]()
-private let erroneousAuthorName = "Unknown"
-private let erroneousAuthorInitials = "U"
+private let erroneousAuthorName = "? Unknown"
+private let erroneousAuthorInitials = "?"
 
 extension NSManagedObject {
     var share: CKShare? {
         try? dataController.container.fetchShares(matching: [self.objectID]).first?.value
     }
-    
-    var userIdentities: [CKUserIdentity] {
-        share?.participants.map { participant in
-            participant.userIdentity
-        } ?? erroneousUserIdentities
+
+    private var userIdentities: [CKUserIdentity] {
+        share?.participants
+            .filter { participant in
+                participant.userIdentity.userRecordID?.recordName != "__defaultOwner__"
+            }
+            .filter { participant in
+                participant.acceptanceStatus == .accepted
+            }
+            .map { participant in
+                participant.userIdentity
+            }
+        ?? erroneousUserIdentities
     }
-    
+
     var userNames: [String] {
-        userIdentities.map {
+        ["You"] + userIdentities.map {
             $0.nameComponents?.formatted() ?? erroneousAuthorName
         }
     }
-    
+
     var userInitials: [String] {
-        userNames.map { userName in
+        ["Y"] + userNames.map { userName in
             if let c = userName.first {
                 return String(c)
             }
             return erroneousAuthorInitials
         }
     }
-    
+
     func wrappedAuthorName(_ authorRecordName: String?) -> String {
         guard let authorRecordName else {
             return erroneousAuthorName
@@ -43,7 +51,7 @@ extension NSManagedObject {
         }
         return authorIdentity.nameComponents?.formatted() ?? erroneousAuthorName
     }
-    
+
     func wrappedAuthorInitials(_ authorRecordName: String?) -> String {
         if let c = wrappedAuthorName(authorRecordName).first {
             return String(c)
