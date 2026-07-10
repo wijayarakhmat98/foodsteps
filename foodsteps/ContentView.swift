0e0959f98604a960e417b1f1b5caac30fb5bfec1
HikarusV <nazwapradana.pc@gmail.com>
2026-07-10 13:47:19 +0700

    [feat]: Navigation Stack as Root parent

--- foodsteps/ContentView.swift
+++ foodsteps/ContentView.swift
@@ -1,18 +1,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var router = AppRoute.shared
+    
     var body: some View {
-        TabView {
-            HomeView2()
-                .tabItem {
-                    Label("Home", systemImage: "house.fill")
-                }
+        @Bindable var bindableRouter = router
+        
+        NavigationStack(path: $bindableRouter.path) {
+            TabView {
+                HomeView2()
+                    .tabItem {
+                        Label("Home", systemImage: "house.fill")
+                    }
 
-            TripHistoryView()
-                .tabItem {
-                    Label("Trip", systemImage: "paperplane.fill")
-                }
+                TripHistoryView()
+                    .tabItem {
+                        Label("Trip", systemImage: "paperplane.fill")
+                    }
+                TripView()
+                    .tabItem {
+                        Label("will Trip", systemImage: "ant.circle")
+                    }
+            }
+            .tint(Color(hex: "#FF8F14"))
+            .navigationDestination(for: Route.self) { route in
+                route.destinationView()
+            }
         }
-        .tint(Color(hex: "#FF8F14"))
+//        .navigationBarHidden(true)
     }
+}
+
+#Preview {
+    ContentView()
 }
