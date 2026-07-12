3cafad8f4ea18310498c764a417cd83eec216227
HikarusV <nazwapradana.pc@gmail.com>
2026-07-12 18:39:59 +0700

    [style]: format Waypoint.swift file with proper spacing and comments

--- foodsteps/Models/Waypoint.swift
+++ foodsteps/Models/Waypoint.swift
@@ -1,3 +1,12 @@
+//
+//  Waypoint.swift
+//  foodsteps
+//
+//  Created by Nazwa Sapta Pradana on 12/07/26.
+//
+import SwiftUI
+import MapKit
+
 struct Waypoint: Identifiable {
     var id = UUID()
     let name: String
